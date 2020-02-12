331712d5a3b0ba45530df40ccbfcad9f59373e09
An <aymannasr43@gmail.com>
2020-02-12 11:40:51 +0200

    Init for linked stack

--- Linked-Based-Stack/Lin_stack.cpp
+++ Linked-Based-Stack/Lin_stack.cpp
@@ -4,7 +4,71 @@
  *  Created on: Feb 10, 2020
  *      Author: an
  */
+#include "Lin_stack.h"
+
+void createStack(Stack * const ps){
+	ps->top = NULL;
+	ps->size = 0;
+}
+
+bool pushStack(STACK_ENTRY element,Stack * const ps){
+	StackNode *pn = (StackNode*)malloc(sizeof(StackNode));
+	if(pn)
+	{
+		pn->Entry = element;
+		pn->next = ps->top;
+		ps->top = pn;
+		ps->size++;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void popStack(STACK_ENTRY * const pe,Stack * const ps){
+	StackNode *pTemp = ps->top;
+	*pe = ps->top->Entry ;
+	ps->top = ps->top->next;
+	free(pTemp);
+	ps->size--;
+}
+
+void clearStack(Stack * const ps){
+	StackNode *pn = ps->top;
+	while(pn)
+	{
+		pn = pn->next;
+		free(ps->top);
+		ps->top = pn;
+	}
+}
+
+bool stackFull(Stack * const ps){
+	return 0;
+}
+
+bool stackEmpty(Stack * const ps){
+	return (ps->size == 0) ;
+}
+
+void stackTop(STACK_ENTRY * const pe,Stack * const ps){
+	*pe = ps->top->Entry;
+}
 
 
+
+int stackSize(Stack * const ps){
+	return ps->size ;
+}
+
+void traverseStack(Stack * const ps,void(*pf)(STACK_ENTRY)){
+	StackNode *pn = ps->top;
+	for (int var = 0; var < ps->size; ++var) {
+		(*pf)(pn->Entry);
+		pn = pn->next ;
+	}
+}
 
 
--- Linked-Based-Stack/Lin_stack.h
+++ Linked-Based-Stack/Lin_stack.h
@@ -8,6 +8,35 @@
 #ifndef LINKED_BASED_STACK_LIN_STACK_H_
 #define LINKED_BASED_STACK_LIN_STACK_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
+		/************ PreProcessors ***********/
+#define STACK_ENTRY int
+#define MAX_ENTRY 10
+
+
+		/************ Structures ***********/
+typedef struct StackNode{
+	STACK_ENTRY Entry;
+	StackNode *next;
+};
+typedef struct Stack{
+	StackNode *top;
+	int size;
+};
+
+
+		/************ Function APIs ***********/
+void createStack(Stack * const ps);
+bool pushStack(STACK_ENTRY,Stack * const ps);
+void popStack(STACK_ENTRY * const,Stack * const ps);
+void clearStack(Stack * const ps);
+bool stackFull(Stack * const ps);
+bool stackEmpty(Stack * const ps);
+void stackTop(STACK_ENTRY * const,Stack * const ps);
+int stackSize(Stack * const ps);
+void traverseStack(Stack * const ps,void(*pf)(STACK_ENTRY));
 
 
 
